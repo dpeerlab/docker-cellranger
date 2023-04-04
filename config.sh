8a6300c0da39b3f46522561b95da9b7de206466e
Andrew Moorman <andrewmoormanmskcc@gmail.com>
2023-04-04 17:07:32 -0400

    Updated to CellRanger 7.1.0

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
-version="7.0.0"
-vdj_ref_version="7.0.0"
-site_url="https://support.10xgenomics.com/single-cell-gene-expression/software/downloads/7.0/"
+version="7.1.0"
+vdj_ref_version="7.1.0"
+site_url="https://support.10xgenomics.com/single-cell-gene-expression/software/downloads/7.1/"
 download_url=""
 
 # docker related
